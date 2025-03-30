99ab078dd81d60a4a4a92fda31c3430df605d48d
obszank <obszank@gmail.com>
2025-03-30 17:10:57 +0200

    some commit

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import MainCategories from "../components/MainCategories";
 import FeaturedPosts from "../components/FeaturedPosts";
+import PostList from "../components/PostList";
 
 const Homepage = () => {
   return (
@@ -68,6 +69,10 @@ const Homepage = () => {
       {/* FEATURED POSTS */}
       <FeaturedPosts />
       {/* POST LIST */}
+      <div className="">
+        <h1 className="my-8 text-2xl text-gray-600">Recent Posts</h1>
+        <PostList/>
+      </div>
     </div>
   );
 };
